54563ce01f4d60d196cc63d9c1fd7d4c02496d7c
Adam Kent <adamkent@Adams-MacBook-Pro.local>
2020-10-27 23:48:34 -0700

    fancy mode

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,31 +1,40 @@
 import React, {Component} from 'react';
+import hamburger from '../hamburger.png';
 
 class Header extends Component {
     constructor(){
         super();
         this.state = {
-
+            toggleShow: false
         }
+    }
+
+    toggleMenuFunc = () => {
+        this.setState( prevState => {
+            return {
+                toggleShow: !prevState.toggleShow
+            }
+        })
     }
 
     render(){
         return <header>
         <div>
-            <img class="logo" src="https://www.logolynx.com/images/logolynx/8e/8e509cde090ec53ec7922159435b025a.png"/>
-            <nav class="nav-bar">
+            <img alt="logo" className="logo" src="https://www.logolynx.com/images/logolynx/8e/8e509cde090ec53ec7922159435b025a.png"/>
+            <nav className={`nav-bar ${this.state.toggleShow ? "show" : ""}`}>
                 <ul>
                     <li>
-                        <a href="#">Home</a>
+                        <a href="http://lmgtfy.com">Home</a>
                     </li>
                     <li>
-                        <a href="#">About</a>
+                        <a href="http://lmgtfy.com">About</a>
                     </li>
                     <li>
-                        <a href="#">Contact</a>
+                        <a href="http://lmgtfy.com">Contact</a>
                     </li>
                 </ul>
             </nav>
-            <button id="nav-btn"></button>
+            <input alt="menu-icon" onClick={this.toggleMenuFunc} type="image" src={hamburger} id="nav-btn"/>
         </div>
     </header>
     }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,27 +2,27 @@ import React from 'react';
 import voltron from '../voltron.png';
 
 const Main = () => {
-    return <main>
-    <h1 id="title">Defenders of the Universe</h1>
-    <img id="voltron" src={voltron}/>
-    <div class="lion-container">
-        <div class="lion black">
-            <img src="https://static.wikia.nocookie.net/voltron/images/0/08/Official_-_Black_Lion_standing_pose.png/revision/latest/scale-to-width-down/619?cb=20160627051448"/>
-        </div>
-        <div class="lion red">
-            <img src="https://static.wikia.nocookie.net/voltron/images/7/7c/Official_-_Red_Lion_leaping_pose.png/revision/latest/scale-to-width-down/620?cb=20160627051824"/>
-        </div>
-        <div class="lion blue">
-            <img src="https://static.wikia.nocookie.net/voltron/images/3/36/Official_-_Blue_Lion_leaping_pose.png/revision/latest/scale-to-width-down/620?cb=20160627044939"/>
-        </div>
-        <div class="lion yellow">
-            <img src="https://static.wikia.nocookie.net/voltron/images/4/42/Official_-_Yellow_Lion_leaping_pose.png/revision/latest/scale-to-width-down/620?cb=20160627051538"/>
-        </div>
-        <div class="lion green">
-            <img src="https://static.wikia.nocookie.net/voltron/images/6/68/Official_-_Green_Lion_prowling_pose.png/revision/latest/scale-to-width-down/619?cb=20160627051501"/>
-        </div>
-    </div>
-</main>
-}
+    return (
+        <main>
+            <img alt="voltron" id="voltron" src={voltron}/>
+            <div className="lion-container">
+                <div className="lion black" >
+                    <img alt="lion" src="https://lh3.googleusercontent.com/proxy/3uyhiiqfUKsCpjnW8qHzay_n-54N5J248ksyuK509l-_SNLUrlcUNTWCQntTYfYF8ys_T-oRuBvX_xj4bd0JFQhQ_eUl"/>
+                </div>
+                <div className="lion red">
+                    <img alt="lion" src="https://static.wikia.nocookie.net/voltron/images/7/7c/Official_-_Red_Lion_leaping_pose.png/revision/latest/scale-to-width-down/620?cb=20160627051824"/>
+                </div>
+                <div className="lion blue">
+                    <img alt="lion" src="https://static.wikia.nocookie.net/voltron/images/3/36/Official_-_Blue_Lion_leaping_pose.png/revision/latest/scale-to-width-down/620?cb=20160627044939"/>
+                </div>
+                <div className="lion yellow">
+                    <img alt="lion" src="https://static.wikia.nocookie.net/voltron/images/4/42/Official_-_Yellow_Lion_leaping_pose.png/revision/latest/scale-to-width-down/620?cb=20160627051538"/>
+                </div>
+                <div className="lion green">
+                    <img alt="lion" src="https://static.wikia.nocookie.net/voltron/images/6/68/Official_-_Green_Lion_prowling_pose.png/revision/latest/scale-to-width-down/619?cb=20160627051501"/>
+                </div>
+            </div>
+        </main>
+    )}
 
 export default Main;
